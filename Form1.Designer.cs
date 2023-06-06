94c152a98b678f750731b67aa289528dbf8d365e
meta_sbeu <125193979+metaSbeu@users.noreply.github.com>
2023-06-06 21:12:18 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Сплайновые_кривые
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1600, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 804);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(1560, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удалить последнюю точку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Сплайновые кривые";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Сплайновые_кривые
+{
+    public partial class Form1 : Form
+    {
+        private Graphics G;
+        private PointF[] Arr = new PointF[] { };
+        private Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Gold, 3);
+            G = pictureBox1.CreateGraphics();
+            G.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+        }
+
+        private void Draw()
+        {
+            int j = 0;
+
+            PointF[] result = new PointF[101];
+            for (float t = 0; t < 1; t += 0.01f)
+            {
+                float ytmp = 0;
+                float xtmp = 0;
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    float b = Casteljo(i, Arr.Length - 1, t);
+                    xtmp += Arr[i].X * b;
+                    ytmp += Arr[i].Y * b;
+                }
+                result[j] = new PointF(xtmp, ytmp);
+                j++;
+            }
+            G.DrawLines(pen, result);
+        }
+
+        float Casteljo(int i, int n, float t)
+        {
+            float res = 1;
+            for (int j = 1; j <= i; j++)
+                res *= (float)(n - j + 1) / j;
+            res *= (float)Math.Pow(t, i) * (float)Math.Pow(1 - t, n - i);
+            return res;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            PointF clickedPoint = e.Location;
+            Array.Resize(ref Arr, Arr.Length + 1);
+            Arr[Arr.Length - 1] = clickedPoint;
+            pictureBox1.Refresh();
+            Draw();
+
+            foreach (PointF point in Arr)
+            {
+                G.FillEllipse(Brushes.Black, point.X - 3, point.Y - 3, 6, 6);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Arr.Length > 0)
+            {
+                Array.Resize(ref Arr, Arr.Length - 1);
+                pictureBox1.Refresh();
+                Draw();
+
+                foreach (PointF point in Arr)
+                {
+                    G.FillEllipse(Brushes.Black, point.X - 3, point.Y - 3, 6, 6);
+                }
+
+            }
+        }
+    }
+}
